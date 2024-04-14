612fb013c43fafe49eccf835340c0122f384d11c
Bruno Silva Santana <ibrunossl@outlook.com>
2024-04-14 16:18:12 -0300

    teste: useFeaturedImageGallery

---
+++ src/components/FeaturedImageGallery/FeaturedImageGallery.test.ts
@@ -0,0 +1,27 @@
+import { act, renderHook } from "@testing-library/react";
+import useFeaturedImageGallery from "./useFeaturedImageGallery";
+
+describe("FeaturedImageGallery", () => {
+  it("should update current image index", () => {
+    const { result } = renderHook(useFeaturedImageGallery);
+
+    expect(result.current.currentImageIndex).toBe(0);
+
+    act(() => {
+      result.current.updateCurrentImageIndex(1);
+    });
+
+    expect(result.current.currentImageIndex).toBe(1);
+  });
+
+  it("should update image active", () => {
+    const { result } = renderHook(useFeaturedImageGallery);
+    expect(result.current.imageActive).toBeUndefined();
+
+    act(() => {
+      result.current.updateImageActive({ src: "teste" });
+    });
+
+    expect(result.current.imageActive?.src).toBe("teste");
+  });
+});
