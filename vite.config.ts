0f0127cf3e5934665ac8d6d5a574afbc1e871d76
Bruno Silva Santana <ibrunoss@outlook.com>
2024-04-14 16:37:41 -0300

    fix: vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,4 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    css: true,
-  },
 });
